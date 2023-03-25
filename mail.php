87146f38313c7f7a4b5f56e04cb5b0f3a6744c72
gluetex <emre.ertugg1@gmail.com>
2023-03-25 12:24:29 +0300

    php

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    
+    $to = "youremail@example.com"; // Replace with your email address
+    $subject = "New message from $name";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+    
+    if (mail($to, $subject, $body)) {
+      $response = array("success" => true);
+    } else {
+      $response = array("success" => false);
+    }
+    
+    header("Content-Type: application/json");
+    echo json_encode($response);
+  }
+?>
